a1215aefb32784d0a5047e0b8591fa2bd7b0c43b
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-13 14:00:41 -0300

    aula de for no array

---
+++ for.js
@@ -0,0 +1,12 @@
+const nomeEstudantes = ['joao', 'maria', 'jose', 'fernando', 'lucas', 'andre'];
+
+for(let i = 0; i < 6; i++) { //para ir mais robusto pode colocar em vez de i < 6 eh soh colocar a variavel.lenght
+    console.log(nomeEstudantes[i]);
+}
+
+//let indice = 0;
+
+//while(indice < nomeEstudantes.length){ //enquanto a variavel indice for menor que o tamanho da variavel (5)
+//    console.log(nomeEstudantes[indice]);
+//    indice++;
+//}
