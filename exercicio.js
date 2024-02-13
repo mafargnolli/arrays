bf935940c26169ef5e036363ba0b1b8be305faad
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-13 14:07:48 -0300

    exercicio teste de while com numeros

---
+++ exercicio.js
@@ -0,0 +1,12 @@
+//criando um array de 5 numeros
+
+const numeros = [0, 3, 20, 30, 18];
+
+let i = 0;
+
+//enquanto i for menor do que o tamanho do array
+while(i < numeros.length){
+    //executar isso
+    console.log(numeros[i]);
+    i++;
+}
