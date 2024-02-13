fa7ace9e650ad50e4183bf35e5371b4f1f2a3a9f
Marianne Fargnolli <ma.fargnolli@gmail.com>
2024-02-13 13:52:19 -0300

    treino while

--- arrayex1.js
+++ arrayex1.js
@@ -1,3 +1,18 @@
 nomesPaises = ['Brasil', 'Argentina', 'Chile', 'Uruguai', 'Peru'];
+//inserindo um pais no fim da lista
 nomesPaises.push('EUA');
 console.log(nomesPaises);
+//retirando pais do comeco da lista
+nomesPaises.pop();
+console.log(nomesPaises);
+
+//inserindo pais no inicio da lista
+nomesPaises.unshift('França');
+console.log(nomesPaises);
+//removendo pais no inicio da lista
+nomesPaises.shift();
+console.log(nomesPaises);
+
+//selecionando um item especifico na posicao desejada entre colchetes
+console.log(nomesPaises[0]);
+
---
+++ while.js
@@ -0,0 +1,8 @@
+const nomeEstudantes = ['joao', 'maria', 'jose', 'fernando', 'lucas', 'andre'];
+
+let indice = 0;
+
+while(indice < nomeEstudantes.length){ //enquanto a variavel indice for menor que o tamanho da variavel (5)
+    console.log(nomeEstudantes[indice]);
+    indice++;
+}
